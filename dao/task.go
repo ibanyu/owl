89c68880ecc95782aaac98ecba6baec87e36900a
liushengjie9506 <liushengjie9506@ipalfish.com>
2020-11-24 15:57:44 +0800

    del tx in get operation

--- dao/task.go
+++ dao/task.go
@@ -49,25 +49,21 @@ const listTaskCondition = "name like ? or creator like ?"
 func (TaskDaoImpl) ListTask(page *service.Pagination) ([]task.DbInjectionTask, int, error) {
 	page.Key = "%" + page.Key + "%"
 
-	tx := GetDB().Begin()
 	var count int
-	if err := tx.Model(&task.DbInjectionTask{}).Where(listTaskCondition,
+	if err := GetDB().Model(&task.DbInjectionTask{}).Where(listTaskCondition,
 		page.Key, page.Key).Count(&count).Error; err != nil {
-		tx.Rollback()
 		return nil, 0, err
 	}
 
 	var tasks []task.DbInjectionTask
-	if err := tx.Order("ct desc").Offset(page.Offset).Limit(page.Limit).
+	if err := GetDB().Order("ct desc").Offset(page.Offset).Limit(page.Limit).
 		Find(&tasks, listTaskCondition, page.Key, page.Key).Error; err != nil {
-		tx.Rollback()
 		return nil, 0, err
 	}
 
 	for idx, taskV := range tasks {
-		formattedItems, _, err := getTaskExecItems(tx, &taskV)
+		formattedItems, _, err := getTaskExecItems(GetDB(), &taskV)
 		if err != nil {
-			tx.Rollback()
 			return nil, 0, err
 		}
 
@@ -77,16 +73,16 @@ func (TaskDaoImpl) ListTask(page *service.Pagination) ([]task.DbInjectionTask, i
 	return tasks, count, nil
 }
 
-func getTaskExecItems(tx *gorm.DB, taskP *task.DbInjectionTask) ([]task.DbInjectionExecItem, []task.DbInjectionSubtask, error) {
+func getTaskExecItems(db *gorm.DB, taskP *task.DbInjectionTask) ([]task.DbInjectionExecItem, []task.DbInjectionSubtask, error) {
 	var formattedItems []task.DbInjectionExecItem
 	var subTasks []task.DbInjectionSubtask
-	if err := tx.Find(&subTasks, "task_id = ?", taskP.ID).Error; err != nil {
+	if err := db.Find(&subTasks, "task_id = ?", taskP.ID).Error; err != nil {
 		return nil, nil, err
 	}
 
 	for idx, subTask := range subTasks {
 		var items []task.DbInjectionExecItem
-		if err := tx.Find(&items, "subtask_id = ?", subTask.ID).Error; err != nil {
+		if err := db.Find(&items, "subtask_id = ?", subTask.ID).Error; err != nil {
 			return nil, nil, err
 		}
 		subTasks[idx].ExecItems = items
@@ -102,15 +98,13 @@ func getTaskExecItems(tx *gorm.DB, taskP *task.DbInjectionTask) ([]task.DbInject
 }
 
 func (TaskDaoImpl) GetTask(id int64) (*task.DbInjectionTask, error) {
-	tx := GetDB().Begin()
 	var task task.DbInjectionTask
-	if err := tx.First(&task, "id = ?", id).Error; err != nil {
+	if err := GetDB().First(&task, "id = ?", id).Error; err != nil {
 		return nil, err
 	}
 
-	formattedItems, subTasks, err := getTaskExecItems(tx, &task)
+	formattedItems, subTasks, err := getTaskExecItems(GetDB(), &task)
 	if err != nil {
-		tx.Rollback()
 		return nil, err
 	}
 
