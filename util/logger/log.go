687a92bd5161a030129c4657044f58e89b556b54
guoyueting13784 <guoyueting@ipalfish.com>
2021-01-12 21:21:14 +0800

    update logger

--- util/logger/log.go
+++ util/logger/log.go
@@ -1,19 +1,44 @@
 package logger
 
 import (
-	"log"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var zlog *zap.SugaredLogger
 
+const loggerTimeLayout = "2006-01-02 15:04:05.000"
+
 //todo , if no path ,change to init func
-func InitLog() {
-	logger, err := zap.NewProduction(zap.AddCallerSkip(1))
-	if err != nil {
-		log.Fatal("init zap logger failed: ", err.Error())
+func InitLog(logDir, fileName string, level string) {
+	logFile := logDir + "/" + fileName
+
+	//1024, 0, 0, true, false
+	lumberJackLogger := &lumberjack.Logger{
+		Filename:   logFile,
+		MaxSize:    1024,
+		MaxBackups: 0,
+		MaxAge:     0,
+		LocalTime:  true,
+		Compress:   false,
 	}
+
+	encoderConfig := zap.NewProductionEncoderConfig()
+	// 格式化时间
+	encoderConfig.EncodeTime = func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.Format(loggerTimeLayout))
+	}
+
+	encoder := zapcore.NewJSONEncoder(encoderConfig)
+	writeSyncer := zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout), zapcore.AddSync(lumberJackLogger))
+	core := zapcore.NewCore(encoder, writeSyncer, ConvertLevel(level))
+
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
 	zlog = logger.Sugar()
 }
 
@@ -51,4 +76,25 @@ func Errorf(template string, args ...interface{}) {
 
 func Errorw(msg string, keysAndValues ...interface{}) {
 	zlog.Errorw(msg, keysAndValues...)
+}
+
+// ConvertLevel convert string level to Level
+func ConvertLevel(level string) zapcore.Level {
+	level = strings.ToLower(level)
+	switch level {
+	case "debug":
+		return zap.DebugLevel
+	case "info":
+		return zap.InfoLevel
+	case "warn":
+		return zap.WarnLevel
+	case "error":
+		return zap.ErrorLevel
+	case "fatal":
+		return zap.FatalLevel
+	case "panic":
+		return zap.PanicLevel
+	default:
+		return zap.InfoLevel
+	}
 }
